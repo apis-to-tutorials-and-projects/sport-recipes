b8aa32daecc151e984649feafd95911572b11d99
X72579MU <105705996+anartzdev@users.noreply.github.com>
2024-11-05 07:22:32 +0100

    feat: working with data

--- src/app/pages/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/pages/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RecipesService } from '../../recipes.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { forkJoin, map, of, switchMap } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipes-detail',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './recipes-detail.component.html',
   styleUrl: './recipes-detail.component.css'
 })
-export class RecipesDetailComponent {
+export class RecipesDetailComponent implements OnInit{
+  private recipes = inject(RecipesService);
+  private route = inject(ActivatedRoute);
+  recipesList = undefined;
+
+  constructor() {
+    this.recipes.recipesList$.subscribe((data) => {
+      this.recipesList = data;
+    })
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("DATAAAAAAAAAAAAAAAAAAAAA")
+    this.route.params.pipe( switchMap((value) => {
+      console.log(value)
+      return forkJoin({
+        selectType: of(value['id']),
+        drinks: this.recipes.getSelectDrinksByType(value['id'])
+      }).pipe(
+        map((data) => {
+          console.log(data)
+          return data;
+        })
+      );
+    })).subscribe((data) => {
+      console.log(data)
+    });
+   
+  }
 
 }
--- src/app/pages/recipes/recipes-list/recipes-list.component.ts
+++ src/app/pages/recipes/recipes-list/recipes-list.component.ts
@@ -1,31 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { RecipesService } from '../../recipes.service';
 import { JsonPipe } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-list',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, RouterModule],
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent {
   private recipes = inject(RecipesService);
+  private _router = inject(Router);
   recipesList: any;
   recipe: any;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.recipes.getDrinks().subscribe((data) => {
+    this.recipes.getDrinksTypes().subscribe((data) => {
+      this.recipes.setList(data);
       console.log('===========');
       console.log(data);
       this.recipesList = data.recipes;
     })
   }
 
-  viewDetails() {
+  viewDetails(recipe: any) {
     // Lógica para redirigir a la vista de detalles (ejemplo de uso de rutas o servicios)
-    console.log('Detalles de la receta:', this.recipe);
+    console.log('Detalles de la receta:', recipe);
+    this._router.navigate(['recipes', recipe.id])
   }
 }
